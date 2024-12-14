ac2062ad2a47e976c3f8e54a3d33136953eb831d
albertegi <albertegi90@gmail.com>
2024-12-14 00:13:14 +0000

    connecting to postgres db

--- app/main.py
+++ app/main.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+import os
+import time
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 app = FastAPI()
 
@@ -10,7 +19,24 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
 
 
 my_posts = [{"title":"First post", "content":"First post content", "id":1},
@@ -18,13 +44,14 @@ my_posts = [{"title":"First post", "content":"First post content", "id":1},
             {"title":"Zoo Life", "content":"Javan Eagle is an endangered specie", "id":3}, ]
 
 
-# python code to retrieve individual post
+# python code to retrieve individual post by id
 
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
-        
+
+# python code to delete  a post by index   
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
